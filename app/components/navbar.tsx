b50ca6587be55acadd836a91531ea8c70278f98f
bshongwe <dru.da.beat.hustle@gmail.com>
2024-09-11 23:03:52 +0200

    Update: icon and theme selector changes

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -60,7 +60,7 @@ const Navbar = () => {
                         className='inline-flex h-10 w-10 items-center justify-center rounded-md border border-input bg-background/30 text-sm font-medium ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:border-white/10'
                         aria-label='my shopping cart'
                     >
-                        <ShoppingCartIcon className='h-5 w-5' />
+                        <GithubIcon className='h-5 w-5' />
                     </a>
                     <Select
                         onValueChange={(theme: ThemeName) => {
@@ -70,7 +70,7 @@ const Navbar = () => {
                         value={theme}
                     >
                         <SelectTrigger
-                            className='w-[180px]'
+                            className='w-[70px]'
                             aria-label='customize theme'
                         >
                             <SelectValue placeholder='Customize' />
