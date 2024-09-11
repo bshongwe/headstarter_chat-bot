159432bef0c326e1526e2fa159d83db98b1b410d
bshongwe <dru.da.beat.hustle@gmail.com>
2024-09-11 22:28:49 +0200

    Debug build error

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -126,11 +126,8 @@ export default function Index() {
         <div className='dark:bg-[radial-gradient(ellipse_80%_50%_at_50%_-20%,hsla(var(--primary)_/_30%),#ffffff00)]'>
             <Navbar />
             <Hero />
-            <Cms />
             <Features />
-            <Pricing />
             <Banner />
-            <FAQs />
             <Contact />
             <Footer />
         </div>
